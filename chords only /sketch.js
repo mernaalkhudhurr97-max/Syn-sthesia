dd4de5213aa74a77761a902d9502bb16e0681028
jakemossantonini <jmos0107@uni.sydney.edu.au>
2026-05-10 18:03:00 +1000

    xhord only statr

---
+++ chords only /sketch.js
@@ -0,0 +1,13 @@
+
+
+
+function chordinfo {
+
+major (0 , 4 , 7 ) ,
+minor (0 , 3 , 7 ) ,
+diminished (0 , 3 , 6 ) ,
+augmented (0 , 4 , 8 ) ,
+suspended2 (0 , 2 , 7 ) ,
+suspended4 (0 , 5 , 7 ) ,
+ 
+}
