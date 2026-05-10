aefcc0e4807a91986aea019aa3dca17178e3e3c3
jakemossantonini <jmos0107@uni.sydney.edu.au>
2026-05-10 18:04:57 +1000

    Update sketch.js

--- chords only /sketch.js
+++ chords only /sketch.js
@@ -4,7 +4,13 @@
 function chordinfo {
 
 major (0 , 4 , 7 ) ,
+major7 (0 , 4 , 7 , 11 ) ,
+major9 (0 , 4 , 7 , 11 , 14 ) ,
 minor (0 , 3 , 7 ) ,
+minor7 (0 , 3 , 7 , 10 ) ,
+minor9 (0 , 3 , 7 , 10 , 14 ) ,
+dom7 (0 , 4 , 7 , 10 ) ,
+dom9 (0 , 4 , 7 , 10 , 14 ) ,
 diminished (0 , 3 , 6 ) ,
 augmented (0 , 4 , 8 ) ,
 suspended2 (0 , 2 , 7 ) ,
