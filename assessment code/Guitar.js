f26fbdce6e2b37bb8bbea2a62f095901405640b9
jakemossantonini <jmos0107@uni.sydney.edu.au>
2026-05-25 11:41:30 +1000

    notes idk if this work we weill see

--- assessment code/Guitar.js
+++ assessment code/Guitar.js
@@ -23,3 +23,66 @@ function setup() {
   loadMidiFile("assets/Guitar Guitar 3.mid");
   loadMidiFile("assets/Guitar Guitar 4.mid");
 }
+
+
+// idk if this is going to work 
+let guitarNotes = {
+  C1: "off",
+  Cs1: "off",
+  D1: "off",
+  Ds1: "off",
+  E1: "off",
+  F1: "off",
+  Fs1: "off",
+  G1: "off",
+  Gs1: "off",
+  A1: "off",
+  As1: "off",
+  B1: "off",
+
+  C2: "off",
+  Cs2: "off",
+  D2: "off",
+  Ds2: "off",
+  E2: "off",
+  F2: "off",
+  Fs2: "off",
+  G2: "off",
+  Gs2: "off",
+  A2: "off",
+  As2: "off",
+  B2: "off",
+
+  C3: "off"
+};
+
+// MIDI note number to output name
+let bassMap = {
+  36: "C1",
+  37: "Cs1",
+  38: "D1",
+  39: "Ds1",
+  40: "E1",
+  41: "F1",
+  42: "Fs1",
+  43: "G1",
+  44: "Gs1",
+  45: "A1",
+  46: "As1",
+  47: "B1",
+
+  48: "C2",
+  49: "Cs2",
+  50: "D2",
+  51: "Ds2",
+  52: "E2",
+  53: "F2",
+  54: "Fs2",
+  55: "G2",
+  56: "Gs2",
+  57: "A2",
+  58: "As2",
+  59: "B2",
+
+  60: "C3"
+};
