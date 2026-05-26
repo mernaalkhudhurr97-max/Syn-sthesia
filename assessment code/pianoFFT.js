7ea0e1866fb40369a2506dbd87f437692dd3d489
jakemossantonini <jmos0107@uni.sydney.edu.au>
2026-05-26 14:55:13 +1000

    Update pianoFFT.js

--- assessment code/pianoFFT.js
+++ assessment code/pianoFFT.js
@@ -1,24 +1,94 @@
 
+let snythFFT1Sounds = [];
+let snythFFT2Sounds = [];
 
-let SnythFFT2Sounds = [];
 
-let fft;
+
+let synthFFt1;
+let synthFFt2;
+
+let synthFFTIsPlaying = false;
 let smoothing = 0.8;
 let numbins = 1024;
 
 
 
+
+
+
+
+
+// was doing it wrong i think you have to set the thing to 0 just like the background in the ed ones idk 
+
+let synth1Spectrum = [];
+let synth1CentroidFreq = 0;
+
+let synth1SubBassEnergy = 0;
+let synth1BassEnergy = 0;
+let synth1LowMidEnergy = 0;
+let synth1MidEnergy = 0;
+let synth1PresenceEnergy = 0;
+let synth1HighEnergy = 0;
+
+
+let synth2Spectrum = [];
+let synth2CentroidFreq = 0;
+
+let synth2SubBassEnergy = 0;
+let synth2BassEnergy = 0;
+let synth2LowMidEnergy = 0;
+let synth2MidEnergy = 0;
+let synth2PresenceEnergy = 0;
+let synth2HighEnergy = 0;
+
+
+
 function preload() {
   
-  SnythFFT2Sounds[0] = loadSound("assets/SnythFFT2/SnythFFT2.wav");
+  snythFFT1Sounds = loadSound("assets/SnythFFT1/SnythFFT1.wav");
+  snythFFT2Sounds = loadSound("assets/SnythFFT2/SnythFFT2.wav");
+ 
 }
 
-fft = new p5.FFT(smoothing,numbins);
+
+/// https://p5js.org/reference/p5.Amplitude/setInput/ ///
 
 
-let lowbassamplitude = fft.getEnergy(20, 600);
-let bassamplitude = fft.getEnergy(600, 800);
-let lowmidbassamplitude = fft.getEnergy(800, 1200);
-let midamplitude = fft.getEnergy(1200, 2000);
-let trebleamplitude = fft.getEnergy(2000, 8000);
-let presenceamplitude = fft.getEnergy(8000, 15000);
+function setupSnythFFTinputs() {
+
+synthFFt1 = new p5.FFT(smoothing,numbins);
+synthFFt2 = new p5.FFT(smoothing,numbins);
+
+synthFFt1.setInput(snythFFT1Sounds);
+synthFFt2.setInput(snythFFT2Sounds);
+
+}
+
+
+
+// surely it work the same as the others just with the different names and stuff
+
+function playGuitar() {
+    guitarSongs.stop();
+  
+
+  guitarSongs.play();
+  guitarIsPlaying = true;
+}
+  
+
+function pauseGuitar() {
+    guitarSongs.pause();
+
+  guitarIsPlaying = false;
+}
+
+
+function setGuitarVolume() {
+  if (guitarMuted) {
+    guitarSongs.setVolume(0, guitarFadeTime);
+  } else {
+    guitarSongs.setVolume(1, guitarFadeTime);
+  }
+}
+
